b39bc1c3a7dda1261e5814c55e1a728d4334e672
Leo Thiessen <leo@visionsencoded.com>
2016-04-06 18:35:58 -0500

    Updated algorithm in AEMeteringModule.m

--- TheAmazingAudioEngine/Modules/Taps/AEMeteringModule.m
+++ TheAmazingAudioEngine/Modules/Taps/AEMeteringModule.m
@@ -42,30 +42,24 @@ static void AEMeteringModuleProcess(__unsafe_unretained AEMeteringModule * THIS,
                                         const AERenderContext * _Nonnull context) {
     const AudioBufferList * abl = AEBufferStackGet(context->stack, 0);
     if ( !abl ) return;
-    // Get "Stereo" peak and average (channels 0 and 1 assumed for this)
-    float avg,  avgL  = 0.0f, avgR  = 0.0f;
-    float peak, peakL = 0.0f, peakR = 0.0f;
-    for ( int i=0; i < abl->mNumberBuffers; i++ ) {
-        // "Left" Channel
-        avg = peak = 0.0f;
-        vDSP_meamgv((float*)abl->mBuffers[i].mData, 1, &avg,  context->frames);
-        vDSP_maxmgv((float*)abl->mBuffers[i].mData, 1, &peak, context->frames);
-        if ( peak > peakL ) peakL = peak;
-        avgL += avg;
-        
-        // "Right" Channel
-        if ( abl->mBuffers[i].mNumberChannels > 1) {
-            avg = peak = 0.0f;
-            vDSP_meamgv((float*)abl->mBuffers[i].mData, 1, &avg,  context->frames);
-            vDSP_maxmgv((float*)abl->mBuffers[i].mData, 1, &peak, context->frames);
-        }
-        if ( peak > peakR ) peakR = peak;
-        avgR += avg;
+    
+    // "Left" Channel
+    if ( abl->mNumberBuffers > 0 ) {
+        float avg  = 0.0f, peak = 0.0f;
+        vDSP_meamgv((float*)abl->mBuffers[0].mData, 1, &avg,  context->frames);
+        vDSP_maxmgv((float*)abl->mBuffers[0].mData, 1, &peak, context->frames);
+        THIS->_lastPeakPower[0] = peak;
+        THIS->_lastAvgPower[0] = avg;
     }
-    THIS->_lastPeakPower[0] = peakL;
-    THIS->_lastPeakPower[1] = peakR;
-    THIS->_lastAvgPower[0]  = avgL / ((float)abl->mNumberBuffers);
-    THIS->_lastAvgPower[1]  = avgR / ((float)abl->mNumberBuffers);
+    
+    // "Right" Channel
+    if ( abl->mNumberBuffers > 1 ) {
+        float avg  = 0.0f, peak = 0.0f;
+        vDSP_meamgv((float*)abl->mBuffers[1].mData, 1, &avg,  context->frames);
+        vDSP_maxmgv((float*)abl->mBuffers[1].mData, 1, &peak, context->frames);
+        THIS->_lastPeakPower[1] = peak;
+        THIS->_lastAvgPower[1] = avg;
+    }
 }
 
 @end
